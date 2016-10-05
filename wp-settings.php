1344fbc1c9e5eb1d9d2f9ef20ad2c9d5940bbc78
Kite <ixkaito@gmail.com>
2016-10-06 05:08:35 +0900

    `wp_plugin_directory_constants()`

--- wp-settings.php
+++ wp-settings.php
@@ -48,124 +48,122 @@ wp_unregister_GLOBALS();
 // Standardize $_SERVER variables across setups.
 wp_fix_server_vars();
 
-var_dump(__FILE__);
+// Check if we have received a request due to missing favicon.ico
+wp_favicon_request();
 
-// // Check if we have received a request due to missing favicon.ico
-// wp_favicon_request();
+// Check if we're in maintenance mode.
+wp_maintenance();
 
-// // Check if we're in maintenance mode.
-// wp_maintenance();
+// Start loading timer.
+timer_start();
 
-// // Start loading timer.
-// timer_start();
+// Check if we're in WP_DEBUG mode.
+wp_debug_mode();
 
-// // Check if we're in WP_DEBUG mode.
-// wp_debug_mode();
+// For an advanced caching plugin to use. Uses a static drop-in because you would only want one.
+if ( WP_CACHE )
+	WP_DEBUG ? include( WP_CONTENT_DIR . '/advanced-cache.php' ) : @include( WP_CONTENT_DIR . '/advanced-cache.php' );
 
-// // For an advanced caching plugin to use. Uses a static drop-in because you would only want one.
-// if ( WP_CACHE )
-// 	WP_DEBUG ? include( WP_CONTENT_DIR . '/advanced-cache.php' ) : @include( WP_CONTENT_DIR . '/advanced-cache.php' );
+// Define WP_LANG_DIR if not set.
+wp_set_lang_dir();
 
-// // Define WP_LANG_DIR if not set.
-// wp_set_lang_dir();
+// Load early WordPress files.
+require( ABSPATH . WPINC . '/compat.php' );
+require( ABSPATH . WPINC . '/functions.php' );
+require( ABSPATH . WPINC . '/class-wp.php' );
+require( ABSPATH . WPINC . '/class-wp-error.php' );
+require( ABSPATH . WPINC . '/plugin.php' );
+require( ABSPATH . WPINC . '/pomo/mo.php' );
 
-// // Load early WordPress files.
-// require( ABSPATH . WPINC . '/compat.php' );
-// require( ABSPATH . WPINC . '/functions.php' );
-// require( ABSPATH . WPINC . '/class-wp.php' );
-// require( ABSPATH . WPINC . '/class-wp-error.php' );
-// require( ABSPATH . WPINC . '/plugin.php' );
-// require( ABSPATH . WPINC . '/pomo/mo.php' );
+// Include the wpdb class and, if present, a db.php database drop-in.
+require_wp_db();
 
-// // Include the wpdb class and, if present, a db.php database drop-in.
-// require_wp_db();
+// Set the database table prefix and the format specifiers for database table columns.
+$GLOBALS['table_prefix'] = $table_prefix;
+wp_set_wpdb_vars();
 
-// // Set the database table prefix and the format specifiers for database table columns.
-// $GLOBALS['table_prefix'] = $table_prefix;
-// wp_set_wpdb_vars();
+// Start the WordPress object cache, or an external object cache if the drop-in is present.
+wp_start_object_cache();
 
-// // Start the WordPress object cache, or an external object cache if the drop-in is present.
-// wp_start_object_cache();
+// Attach the default filters.
+require( ABSPATH . WPINC . '/default-filters.php' );
 
-// // Attach the default filters.
-// require( ABSPATH . WPINC . '/default-filters.php' );
+// Initialize multisite if enabled.
+if ( is_multisite() ) {
+	require( ABSPATH . WPINC . '/ms-blogs.php' );
+	require( ABSPATH . WPINC . '/ms-settings.php' );
+} elseif ( ! defined( 'MULTISITE' ) ) {
+	define( 'MULTISITE', false );
+}
 
-// // Initialize multisite if enabled.
-// if ( is_multisite() ) {
-// 	require( ABSPATH . WPINC . '/ms-blogs.php' );
-// 	require( ABSPATH . WPINC . '/ms-settings.php' );
-// } elseif ( ! defined( 'MULTISITE' ) ) {
-// 	define( 'MULTISITE', false );
-// }
+register_shutdown_function( 'shutdown_action_hook' );
 
-// register_shutdown_function( 'shutdown_action_hook' );
+// Stop most of WordPress from being loaded if we just want the basics.
+if ( SHORTINIT )
+	return false;
 
-// // Stop most of WordPress from being loaded if we just want the basics.
-// if ( SHORTINIT )
-// 	return false;
+// Load the L10n library.
+require_once( ABSPATH . WPINC . '/l10n.php' );
 
-// // Load the L10n library.
-// require_once( ABSPATH . WPINC . '/l10n.php' );
+// Run the installer if WordPress is not installed.
+wp_not_installed();
 
-// // Run the installer if WordPress is not installed.
-// wp_not_installed();
+// Load most of WordPress.
+require( ABSPATH . WPINC . '/class-wp-walker.php' );
+require( ABSPATH . WPINC . '/class-wp-ajax-response.php' );
+require( ABSPATH . WPINC . '/formatting.php' );
+require( ABSPATH . WPINC . '/capabilities.php' );
+require( ABSPATH . WPINC . '/query.php' );
+require( ABSPATH . WPINC . '/date.php' );
+require( ABSPATH . WPINC . '/theme.php' );
+require( ABSPATH . WPINC . '/class-wp-theme.php' );
+require( ABSPATH . WPINC . '/template.php' );
+require( ABSPATH . WPINC . '/user.php' );
+require( ABSPATH . WPINC . '/session.php' );
+require( ABSPATH . WPINC . '/meta.php' );
+require( ABSPATH . WPINC . '/general-template.php' );
+require( ABSPATH . WPINC . '/link-template.php' );
+require( ABSPATH . WPINC . '/author-template.php' );
+require( ABSPATH . WPINC . '/post.php' );
+require( ABSPATH . WPINC . '/post-template.php' );
+require( ABSPATH . WPINC . '/revision.php' );
+require( ABSPATH . WPINC . '/post-formats.php' );
+require( ABSPATH . WPINC . '/post-thumbnail-template.php' );
+require( ABSPATH . WPINC . '/category.php' );
+require( ABSPATH . WPINC . '/category-template.php' );
+require( ABSPATH . WPINC . '/comment.php' );
+require( ABSPATH . WPINC . '/comment-template.php' );
+require( ABSPATH . WPINC . '/rewrite.php' );
+require( ABSPATH . WPINC . '/feed.php' );
+require( ABSPATH . WPINC . '/bookmark.php' );
+require( ABSPATH . WPINC . '/bookmark-template.php' );
+require( ABSPATH . WPINC . '/kses.php' );
+require( ABSPATH . WPINC . '/cron.php' );
+require( ABSPATH . WPINC . '/deprecated.php' );
+require( ABSPATH . WPINC . '/script-loader.php' );
+require( ABSPATH . WPINC . '/taxonomy.php' );
+require( ABSPATH . WPINC . '/update.php' );
+require( ABSPATH . WPINC . '/canonical.php' );
+require( ABSPATH . WPINC . '/shortcodes.php' );
+require( ABSPATH . WPINC . '/class-wp-embed.php' );
+require( ABSPATH . WPINC . '/media.php' );
+require( ABSPATH . WPINC . '/http.php' );
+require( ABSPATH . WPINC . '/class-http.php' );
+require( ABSPATH . WPINC . '/widgets.php' );
+require( ABSPATH . WPINC . '/nav-menu.php' );
+require( ABSPATH . WPINC . '/nav-menu-template.php' );
+require( ABSPATH . WPINC . '/admin-bar.php' );
 
-// // Load most of WordPress.
-// require( ABSPATH . WPINC . '/class-wp-walker.php' );
-// require( ABSPATH . WPINC . '/class-wp-ajax-response.php' );
-// require( ABSPATH . WPINC . '/formatting.php' );
-// require( ABSPATH . WPINC . '/capabilities.php' );
-// require( ABSPATH . WPINC . '/query.php' );
-// require( ABSPATH . WPINC . '/date.php' );
-// require( ABSPATH . WPINC . '/theme.php' );
-// require( ABSPATH . WPINC . '/class-wp-theme.php' );
-// require( ABSPATH . WPINC . '/template.php' );
-// require( ABSPATH . WPINC . '/user.php' );
-// require( ABSPATH . WPINC . '/session.php' );
-// require( ABSPATH . WPINC . '/meta.php' );
-// require( ABSPATH . WPINC . '/general-template.php' );
-// require( ABSPATH . WPINC . '/link-template.php' );
-// require( ABSPATH . WPINC . '/author-template.php' );
-// require( ABSPATH . WPINC . '/post.php' );
-// require( ABSPATH . WPINC . '/post-template.php' );
-// require( ABSPATH . WPINC . '/revision.php' );
-// require( ABSPATH . WPINC . '/post-formats.php' );
-// require( ABSPATH . WPINC . '/post-thumbnail-template.php' );
-// require( ABSPATH . WPINC . '/category.php' );
-// require( ABSPATH . WPINC . '/category-template.php' );
-// require( ABSPATH . WPINC . '/comment.php' );
-// require( ABSPATH . WPINC . '/comment-template.php' );
-// require( ABSPATH . WPINC . '/rewrite.php' );
-// require( ABSPATH . WPINC . '/feed.php' );
-// require( ABSPATH . WPINC . '/bookmark.php' );
-// require( ABSPATH . WPINC . '/bookmark-template.php' );
-// require( ABSPATH . WPINC . '/kses.php' );
-// require( ABSPATH . WPINC . '/cron.php' );
-// require( ABSPATH . WPINC . '/deprecated.php' );
-// require( ABSPATH . WPINC . '/script-loader.php' );
-// require( ABSPATH . WPINC . '/taxonomy.php' );
-// require( ABSPATH . WPINC . '/update.php' );
-// require( ABSPATH . WPINC . '/canonical.php' );
-// require( ABSPATH . WPINC . '/shortcodes.php' );
-// require( ABSPATH . WPINC . '/class-wp-embed.php' );
-// require( ABSPATH . WPINC . '/media.php' );
-// require( ABSPATH . WPINC . '/http.php' );
-// require( ABSPATH . WPINC . '/class-http.php' );
-// require( ABSPATH . WPINC . '/widgets.php' );
-// require( ABSPATH . WPINC . '/nav-menu.php' );
-// require( ABSPATH . WPINC . '/nav-menu-template.php' );
-// require( ABSPATH . WPINC . '/admin-bar.php' );
+// Load multisite-specific files.
+if ( is_multisite() ) {
+	require( ABSPATH . WPINC . '/ms-functions.php' );
+	require( ABSPATH . WPINC . '/ms-default-filters.php' );
+	require( ABSPATH . WPINC . '/ms-deprecated.php' );
+}
 
-// // Load multisite-specific files.
-// if ( is_multisite() ) {
-// 	require( ABSPATH . WPINC . '/ms-functions.php' );
-// 	require( ABSPATH . WPINC . '/ms-default-filters.php' );
-// 	require( ABSPATH . WPINC . '/ms-deprecated.php' );
-// }
-
-// // Define constants that rely on the API to obtain the default value.
-// // Define must-use plugin directory constants, which may be overridden in the sunrise.php drop-in.
-// wp_plugin_directory_constants();
+// Define constants that rely on the API to obtain the default value.
+// Define must-use plugin directory constants, which may be overridden in the sunrise.php drop-in.
+wp_plugin_directory_constants();
 
 // $GLOBALS['wp_plugin_paths'] = array();
 
